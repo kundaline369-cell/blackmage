d8be5adf6e476212afe28f3d6f2d203689577143
kundaline369-cell <kundaline369@gmail.com>
2026-03-22 04:38:04 -0500

    fix port

--- time_panel.py
+++ time_panel.py
@@ -21,3 +21,6 @@ def index():
     return render_template_string(HTML, resposta=resposta)
 
 app.run(host="0.0.0.0", port=8080)
+import os
+port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port)
